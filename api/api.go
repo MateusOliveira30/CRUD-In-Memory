6e01663906d82763bd2b71a503d35877961eb5e2
MateusOliveira30 <mcesaroliveira30@yahoo.com>
2024-08-29 19:03:21 -0300

    feat: creating routes with chiV5

--- api/api.go
+++ api/api.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewHandler(db map[string]string) http.Handler {
+
+	r := chi.NewMux()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	r.Get("/api/users")
+	r.Get("/api/users/:id")
+	r.Delete("/api/users/:id")
+	r.Post("/api/users")
+	r.Put("/api/users/:id")
+
+	return r
+}
